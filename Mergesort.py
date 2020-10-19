46e783e81887550f8560bcc79611ef1f471446e1
fightnyy <fightnyy@naver.com>
2020-10-19 23:35:56 +0900

    MergeSort

--- Mergesort.py
+++ Mergesort.py
@@ -17,23 +17,25 @@ def merge(left , right) :
 
     if j == len(right) :
         v = v+left[i:len(left)]
-        
+    print("merge 함수의 V : ",v)
+    print("merge V의 id",id(v))
     return v
 
 def merge_sort(v):
     if len(v) <=1 :
+        print("v : ",v)
         return v
     m = len(v)//2
     left = merge_sort(v[0:m])
     right = merge_sort(v[m:len(v)])
+    print("left : ",left , "right : ",right)
     return merge(left, right)
 
 
 
 def main ():
     S = [8,7,6,5,4,3,2,1]
-    q = [10,11,9]
-    print("Merged : ",merge_sort(S+q))
+    print("Merged : ",merge_sort(S))
 
 if __name__ == "__main__":
     main()
