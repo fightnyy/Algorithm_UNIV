7c1d91689dd0b0105c98a77dd2133b3fccb52c06
fightnyy <fightnyy@naver.com>
2020-10-20 21:25:02 +0900

    Quick

---
+++ quick_sort.py
@@ -0,0 +1,25 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)//2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else :
+            equal_arr.append(num)
+
+    return quick_sort(lesser_arr)+equal_arr+quick_sort(greater_arr)
+
+
+def main ():
+    arr = [8,7,6,5,4,3,2,1]
+    s_arr = quick_sort(arr)
+    print(s_arr)
+
+
+
+if __name__ == "__main__":
+    main()
